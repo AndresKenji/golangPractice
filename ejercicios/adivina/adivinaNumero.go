a28ea35fbdc36a624ea64c5d5473f595601054a8
Oscar Rodriguez <52334508+AndresKenji@users.noreply.github.com>
2024-03-14 17:33:39 -0500

    ejercicios udemy

---
+++ ejercicios/adivina/adivinaNumero.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	numeroMeta := rand.Intn(100)
+	intentos := 1
+	var numero int
+	ganador := false
+	fmt.Println("Adivina el numero de 0 a 100 antes de quedarte sin intentos")
+	for intentos <= 10 || ganador == true {
+		fmt.Println("Cantidad de intentos:",intentos)
+		fmt.Print("Adivina el numero:")
+		fmt.Scanln(&numero)
+		if numero == numeroMeta {
+			fmt.Printf("Lo lograste !! en %v intentos, que barbaro ! 😲",intentos)
+			ganador = true
+			return
+		}else {
+			if numero < numeroMeta {
+				fmt.Println("Un poco mas")
+			}
+			if numero > numeroMeta {
+				fmt.Println("Un poco menos")
+			}
+		}		
+		intentos ++
+	}
+
+	fmt.Println("Que lastima, eso de las matematicas no es lo tuyo 😅")
+	
+
+}
---
+++ ejercicios/areaTriangulo/triangulo.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var A, B float64
+	const precision = 2
+	
+	fmt.Println("Hola, vamos a calcular la hipotenusa y el area del triangulo rectangulo, para eso ingresa lo siguiente:")
+	fmt.Print("Introduce el lado A del triangulo: ")
+	fmt.Scanln(&A)
+	fmt.Print("Introduce el lado B del triangulo: ")
+	fmt.Scanln(&B)
+
+	hipotenusa := math.Sqrt(math.Pow(A,2) + math.Pow(B,2))
+	area := (A * B)/2
+	perimetro := hipotenusa + A + B
+
+	fmt.Printf("Los valores ingresados son A: %v, B: %v \n", A, B)
+	fmt.Printf("La hipotenusa es: %v \n", hipotenusa)
+	fmt.Printf("El area es: %.2f \n", area)
+	fmt.Printf("El perimetro es: %.*f \n", precision, perimetro)
+
+}
