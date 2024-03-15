bcff2208333baf1a1fa1f05aeabf8002da8d972a
Kenji <52334508+AndresKenji@users.noreply.github.com>
2024-03-15 07:59:26 -0500

    todo 2

--- ejercicios/todolist/todo.go
+++ ejercicios/todolist/todo.go
@@ -1,18 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type Tarea struct {
 	Id         int
 	Nombre     string
 	Detalle    string
-	Finalizado bool
+	Finalizado bool `default:"false"`
+}
+
+var contadorID int = 1
+var reader = bufio.NewReader(os.Stdin)
+func leerEntrada() string {
+    entrada, _ := reader.ReadString('\n')
+	entrada = strings.TrimSpace(entrada)
+    return entrada
 }
 
 func (t *Tarea) modificarTarea() {
 	var detalleNuevo string
 	fmt.Println("Ingresa el nuevo detalle de la tarea")
-	fmt.Scanln(&detalleNuevo)
+	detalleNuevo = leerEntrada()
 	t.Detalle = detalleNuevo
 	fmt.Println("Detalle modificado")
 }
@@ -26,6 +39,26 @@ func (t *Tarea) finalizar(){
 	}
 }
 
+func agregarTarea(tareas *[]Tarea){
+	var nombre, detalle string
+	fmt.Print("Nombre:")
+	nombre = leerEntrada()
+	fmt.Print("Detalle:")
+	detalle = leerEntrada()
+	nuevaTarea := Tarea {Id:contadorID ,Nombre: nombre,Detalle: detalle} 
+	*tareas = append(*tareas, nuevaTarea)
+	contadorID ++
+}
+
+func listarTareas(tareas *[]Tarea){
+	for _, tarea := range *tareas{
+		fmt.Printf("%v) %s: %s Finalizado: %t \n",tarea.Id, tarea.Nombre, tarea.Detalle, tarea.Finalizado)
+	}
+}
+func remove(slice []Tarea, s int) []Tarea {
+    return append(slice[:s], slice[s+1:]...)
+}
+
 func main() {
 	misTareas := []Tarea{}
 	continuar := true
@@ -33,10 +66,31 @@ func main() {
 	for continuar {
 		fmt.Print(`
 		Menu
+		Por favor elije una opción
 		1) Agregar Tarea
 		2) Lista de Tareas
-		3) Eliminar Tarea
+		3) Finalizar Tarea
+		4) Eliminar Tarea
 		`)
+		var opcion int
+		fmt.Scanln(&opcion)
+		switch opcion {
+		case 1:
+			agregarTarea(&misTareas)
+		case 2:
+			listarTareas(&misTareas)
+		case 3:
+			var idTarea int
+			fmt.Print("Id:")
+			fmt.Scanln(&idTarea)
+			misTareas[idTarea].finalizar()
+		case 4:
+			var idTarea int
+			fmt.Print("Id:")
+			fmt.Scanln(&idTarea)
+			misTareas = remove(misTareas, idTarea)
+		}
+
 	}
 
 
