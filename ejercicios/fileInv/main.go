29aa076df635c6b722bbe1bfc2fdadf0427ad4fe
Kenji <52334508+AndresKenji@users.noreply.github.com>
2024-03-26 08:39:37 -0500

    main

--- ejercicios/fileInv/main.go
+++ ejercicios/fileInv/main.go
@@ -15,8 +15,8 @@ func main() {
 	var route string
 	var outFile string
 
-	flag.StringVar(&route,"root","/", "Ruta a examinar")
-	flag.StringVar(&outFile,"outfile","data", "Nombre del archivo de salida")
+	flag.StringVar(&route, "root", "/", "Ruta a examinar")
+	flag.StringVar(&outFile, "outfile", "data", "Nombre del archivo de salida")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Uso del programa:\n")
 		flag.PrintDefaults()
@@ -29,72 +29,69 @@ func main() {
 		os.Exit(1)
 	}
 
-
-
 	//root := "F:/Kenji"
-	files := make([]fileInfo,0,200)
+	files := make([]fileInfo, 0, 200)
 	saveDirFiles(&files, route)
 
-	fmt.Println("El total de archivos obtenidos es de",len(files))
+	fmt.Println("El total de archivos obtenidos es de", len(files))
 
 	// Crear un nuevo archivo CSV para escribir
-    file, err := os.Create(outFile + ".csv")
-    if err != nil {
-        panic(err)
-    }
-    defer file.Close()
+	file, err := os.Create(outFile + ".csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-    // Crear un escritor CSV
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
+	// Crear un escritor CSV
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
 	// Escribir el encabezado del CSV
-    header := []string{"Full_name", "Name", "size","extension"}
-    if err := writer.Write(header); err != nil {
-        log.Panic(err)
-    }
+	header := []string{"Full_name", "Name", "size", "extension"}
+	if err := writer.Write(header); err != nil {
+		log.Panic(err)
+	}
 
 	// Escribir los datos de cada estructura en el archivo CSV
-    for _, fileInfo := range files {
-        record := []string{
-            fileInfo.FullName,
+	for _, fileInfo := range files {
+		record := []string{
+			fileInfo.FullName,
 			fileInfo.Name,
-            strconv.FormatInt(fileInfo.Size, 10),
+			strconv.FormatInt(fileInfo.Size, 10),
 			fileInfo.Extension,
-        }
-        if err := writer.Write(record); err != nil {
-            panic(err)
-        }
-    }
+		}
+		if err := writer.Write(record); err != nil {
+			panic(err)
+		}
+	}
 
-	
 }
 
-func saveDirFiles(files *[]fileInfo, directory string){
+func saveDirFiles(files *[]fileInfo, directory string) {
 	entries, err := os.ReadDir(directory)
 	if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
 	for _, e := range entries {
-		if e.IsDir(){
-			saveDirFiles(files, filepath.Join(directory,e.Name()) )
-		}else{
+		if e.IsDir() {
+			saveDirFiles(files, filepath.Join(directory, e.Name()))
+		} else {
 			inf, _ := e.Info()
-			var newFile fileInfo 
-			newFile.FullName = filepath.Join(directory,e.Name())
+			var newFile fileInfo
+			newFile.FullName = filepath.Join(directory, e.Name())
 			newFile.Name = e.Name()
 			newFile.Size = inf.Size()
-			newFile.Extension = filepath.Ext(filepath.Join(directory,e.Name()))
+			newFile.Extension = filepath.Ext(filepath.Join(directory, e.Name()))
 
 			*files = append(*files, newFile)
 		}
-    }
+	}
 }
 
 type fileInfo struct {
-	FullName string
-	Name string
-	Size int64
+	FullName  string
+	Name      string
+	Size      int64
 	Extension string
 }
