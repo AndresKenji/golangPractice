ff0802b6f4e6b82479b9e0ca61d5807145b12f73
Oscar Rodriguez <52334508+AndresKenji@users.noreply.github.com>
2024-08-13 17:19:32 -0500

    web app course seccion 5

--- webApplications/01-HelloWorld/cmd/web/main.go
+++ webApplications/01-HelloWorld/cmd/web/main.go
@@ -4,20 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"helloworld/pkg/config"
 	"helloworld/pkg/handlers"
 	"helloworld/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8801"
+var app config.AppConfig
+var session *scs.SessionManager
 
 
 
 
 func main() {
 
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	// session para manejo de sesiones con el paquete github.com/alexedwards/scs/v2
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true // para que la sesión persista incluso despues de cerrar el navegador
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+	
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -28,9 +45,19 @@ func main() {
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", repo.Home)
-	http.HandleFunc("/about", repo.About)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber,nil)
+
+	// http.HandleFunc("/", repo.Home)
+	// http.HandleFunc("/about", repo.About)
+
+	fmt.Printf("Starting application on port %s \n", portNumber)
+	// _ = http.ListenAndServe(portNumber,nil)
+
+	srv := &http.Server{
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
