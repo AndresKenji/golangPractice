ea8ce2c3294c34cca166c1954b029a891a05512e
Oscar Rodriguez <52334508+AndresKenji@users.noreply.github.com>
2024-08-18 17:39:05 -0500

    d

---
+++ webApplications/02-booking/cmd/web/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"bookings/pkg/config"
+	"bookings/pkg/handlers"
+	"bookings/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8801"
+var app config.AppConfig
+var session *scs.SessionManager
+
+
+
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// session para manejo de sesiones con el paquete github.com/alexedwards/scs/v2
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true // para que la sesión persista incluso despues de cerrar el navegador
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+	
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+
+
+	// http.HandleFunc("/", repo.Home)
+	// http.HandleFunc("/about", repo.About)
+
+	fmt.Printf("Starting application on port %s \n", portNumber)
+	// _ = http.ListenAndServe(portNumber,nil)
+
+	srv := &http.Server{
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ webApplications/02-booking/readme.md
@@ -0,0 +1,8 @@
+# Booking and Reservations 
+
+Este repositorio contiene el proyecto Booking and Reservations el cual hace parte del curso **Building Modern Web Applications with Go (Golang)** del profesor Trevor Sawler.
+
+- Construido con Go 1.22
+- Usa [chi router](github.com/go-chi/chi/v5) version 5 para el manejo de rutas
+- Usa [alex scs](github.com/alexedwards/scs/v2) version 2 para el manejo de sesiones
+- Usa [nosurf](github.com/justinas/nosurf) para el manejo de CSRF
